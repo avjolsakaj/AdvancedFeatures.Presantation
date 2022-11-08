a2c6a752c213260183ce0b58f15b9c370abc1a13
Avjol Sakaj <avjol.sakaj@cigen.me>
2022-11-08 09:26:13 +0100

    Exercise Twenty

---
+++ AdvancedFeaturesExerciseTwenty/Joiner.cs
@@ -0,0 +1,22 @@
+﻿namespace AdvancedFeaturesCoding.ExerciseTwenty;
+
+public class Joiner<T>
+{
+    private string _separator;
+    public Joiner (string separator)
+    {
+        _separator = separator;
+    }
+
+    public string Join (IEnumerable<T> values)
+    {
+        var result = string.Empty;
+        foreach (var item in values)
+        {
+            result += item?.ToString() + _separator;
+        }
+        return result.Remove(result.Length - 1);
+
+        //string.Join(_separator, values);
+    }
+}
---
+++ AdvancedFeaturesExerciseTwenty/Program.cs
@@ -0,0 +1,34 @@
+﻿using AdvancedFeaturesCoding.ExerciseTwenty;
+
+namespace AdvancedFeaturesExerciseTwenty
+{
+    internal class Program
+    {
+        private static void Main (string[] args)
+        {
+            var numbers = new List<int>
+            {
+                1, 2, 3, 4, 5, 6, 7
+            };
+
+            var strings = new List<string>
+            {
+                "koli",
+                "enio",
+                "aleks"
+            };
+
+            var joining = new Joiner<int>(",");
+            var result = joining.Join(numbers);
+
+            var joiningString = new Joiner<string>(",");
+            var resultString = joiningString.Join(strings);
+
+            Console.WriteLine(result);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(resultString);
+        }
+    }
+}
