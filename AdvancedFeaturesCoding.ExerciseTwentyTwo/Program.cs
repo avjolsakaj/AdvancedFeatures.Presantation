18b1e9796488be62a3101dca4bca4eb6428dca4a
Avjol Sakaj <avjol.sakaj@cigen.me>
2022-11-08 09:37:27 +0100

    Exercise 22

---
+++ AdvancedFeaturesCoding.ExerciseTwentyTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace AdvancedFeaturesCoding.ExerciseTwentyTwo;
+
+internal class Program
+{
+    private static void Main (string[] args)
+    {
+        var path = @"C:\Users\DELL\Desktop\repository\file.txt";
+
+        var content = File.ReadAllText(path!);
+        var chars = content.ToCharArray();
+
+        var newContent = string.Empty;
+        for (var i = chars.Length - 1; i >= 0; i--)
+        {
+            newContent += chars[i];
+        }
+
+        Console.WriteLine(newContent);
+
+        File.WriteAllText(path!, newContent);
+    }
+}
